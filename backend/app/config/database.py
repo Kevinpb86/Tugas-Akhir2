b54fe063eda3718b0574d0fce07743540fbd497a
Davaihza <dava.ihza18@gmail.com>
2026-06-13 18:50:10 +0700

    refaktor: restrukturisasi backend ke Clean Architecture dan sembunyikan kredensial SMTP di .env

---
+++ backend/app/config/database.py
@@ -0,0 +1,10 @@
+import mysql.connector
+from .db import DB_CONFIG
+
+def get_db():
+    try:
+        conn = mysql.connector.connect(**DB_CONFIG)
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
