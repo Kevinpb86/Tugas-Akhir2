d7318cdec7baea2742ec4f95ebd11a2523297428
Davaihza <dava.ihza18@gmail.com>
2026-07-21 19:52:29 +0700

    Resolve stash pop conflicts

--- amanin/lib/services/api_config.dart
+++ amanin/lib/services/api_config.dart
@@ -6,15 +6,15 @@ class ApiConfig {
   // - Web (Chrome): http://127.0.0.1:8000
   // - HP Android fisik: menggunakan localBackendIp dari api_config_local.dart
   
-  static const String _webUrl = 'http://127.0.0.1:8000';
+  static const String _webUrl = 'https://rpvzwb-ip-180-247-241-88.tunnelmole.net';
   
   // SILAKAN PILIH SALAH SATU URL DI BAWAH (Hanya boleh ada satu yang aktif):
   
   // Pilihan 1: Menggunakan IP Lokal Laptop (untuk koneksi satu jaringan Wi-Fi)
-  static final String _androidUrl = 'http://$localBackendIp:8000';
+  // static final String _androidUrl = 'http://$localBackendIp:8000';
   
   // Pilihan 2: Menggunakan Tunnelmole (untuk koneksi beda jaringan Wi-Fi/Internet seluler)
-  // static final String _androidUrl = 'https://3eeqwi-ip-182-10-132-10.tunnelmole.net'; // Tunnelmole URL
+  static final String _androidUrl = 'https://rpvzwb-ip-180-247-241-88.tunnelmole.net'; // Tunnelmole URL
   
   static String get baseUrl => kIsWeb ? _webUrl : _androidUrl;
 }
